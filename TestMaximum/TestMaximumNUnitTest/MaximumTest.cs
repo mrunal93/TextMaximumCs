a5b7e661a5fc480b7df8ad818d25b2d071b88645
mrunal93 <mrn26893@gmail.com>
2020-07-25 22:35:26 +0530

    [Mrunal]Add: passed Multi parametr and using short had find maximum value

--- TestMaximum/TestMaximumNUnitTest/MaximumTest.cs
+++ TestMaximum/TestMaximumNUnitTest/MaximumTest.cs
@@ -4,72 +4,29 @@ namespace TestMaximumNUnitTest
 {
     public class MaximumTest
     {
-
-
         [Test]
-        public void GivenMaximumNumber_WhenAtFirstPosition_ReturnTrue()
+           public void GivenMaximumNumber_WhenAtFirstPosition_ReturnTrue()
         {
-            TestMaximum.MaximumUtility<int> maximum = new TestMaximum.MaximumUtility<int>(4, 3, 1);
-            int maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual(4, maximumNumber);
+            TestMaximum.MaximumUtility<int> maximum = new TestMaximum.MaximumUtility<int>();
+            int maximumNumber = maximum.FindMaximumNumber(1,2,3,50,60,20);
+            Assert.AreEqual(60, maximumNumber);
         }
         [Test]
         public void GivenMaximumNumber_WhenAtSecondPosition_ReturnTrue()
         {
-            TestMaximum.MaximumUtility<int> maximum = new TestMaximum.MaximumUtility<int>(3, 4, 1);
-            int maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual(4, maximumNumber);
+            TestMaximum.MaximumUtility<float> maximum = new TestMaximum.MaximumUtility<float>();
+            float maximumNumber = maximum.FindMaximumNumber(1f,2f,3f,50f,20f,60f);
+            Assert.AreEqual(60f, maximumNumber);
         }
         [Test]
         public void GivenMaximumNumber_WhenAtThirdPosition_ReturnTrue()
         {
-            TestMaximum.MaximumUtility<float> maximum = new TestMaximum.MaximumUtility<float>(1, 3, 4);
-            float maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual(4f, maximumNumber);
+            TestMaximum.MaximumUtility<string> maximum = new TestMaximum.MaximumUtility<string>();
+            string maximumNumber = maximum.FindMaximumNumber("A","Z","M","N","U");
+            Assert.AreEqual("Z", maximumNumber);
         }
 
-        [Test]
-        public void GivenFlotMaximumNumber_WhenAtFirstPosition_ReturnTrue()
-        {
-            TestMaximum.MaximumUtility<float> maximum = new TestMaximum.MaximumUtility<float>(4f, 3f, 1f);
-            float maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual(4f, maximumNumber);
-        }
-        [Test]
-        public void GivenFlotMaximumNumber_WhenAtSecondPosition_ReturnTrue()
-        {
-            TestMaximum.MaximumUtility<float> maximum = new TestMaximum.MaximumUtility<float>(3f, 4f, 1f);
-            float maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual(4f, maximumNumber);
-        }
-        [Test]
-        public void GivenFlotMaximumNumber_WhenAtThirdPosition_ReturnTrue()
-        {
-            TestMaximum.MaximumUtility<float> maximum = new TestMaximum.MaximumUtility<float>(3f, 1f, 4f);
-            float maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual(4f, maximumNumber);
-        }
-        [Test]
-        public void GivenStringMaximumNumber_WhenAtFirstPosition_ReturnTrue()
-        {
-            TestMaximum.MaximumUtility<string> maximum = new TestMaximum.MaximumUtility<string>("C", "A", "B");
-            string maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual("C", maximumNumber);
-        }
-        [Test]
-        public void GivenStringMaximumNumber_WhenAtSecondPosition_ReturnTrue()
-        {
-            TestMaximum.MaximumUtility<string> maximum = new TestMaximum.MaximumUtility<string>("A","C","B");
-            string maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual("C", maximumNumber);
-        }
-        [Test]
-        public void GivenStringMaximumNumber_WhenAtThirdPosition_ReturnTrue()
-        {
-            TestMaximum.MaximumUtility<string> maximum = new TestMaximum.MaximumUtility<string>("B","C","A");
-            string maximumNumber = maximum.FindMaximumNumber();
-            Assert.AreEqual("C", maximumNumber);
-        }
+        
 
     }
 }
