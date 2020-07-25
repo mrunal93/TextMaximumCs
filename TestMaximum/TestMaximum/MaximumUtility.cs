a43d3de018d8960c0308f3878a504ea8280932e2
mrunal93 <mrn26893@gmail.com>
2020-07-25 07:29:46 +0530

    [Mrunal]Add: Add the String compaired them found maximum number using generic methord

--- TestMaximum/TestMaximum/MaximumUtility.cs
+++ TestMaximum/TestMaximum/MaximumUtility.cs
@@ -8,32 +8,32 @@ namespace TestMaximum
     {
         
 
-        public int FindMaximumNumber(int XNumber, int YNumber, int ZNumber)
+        public E FindMaximumNumber<E>(E XNumber, E YNumber, E ZNumber) where E : IComparable
         {
-            int maximum = XNumber;
+            E maximum = XNumber;
             if (maximum.CompareTo(YNumber) < 0)
                 maximum = YNumber;
             if (maximum.CompareTo(ZNumber) < 0)
                 maximum = ZNumber;
             return maximum;
         }
-        public float FindMaximumNumber(float XNumber, float YNumber, float ZNumber)
-        {
-            float maximum = XNumber;
-            if (maximum.CompareTo(YNumber) < 0)
-                maximum = YNumber;
-            if (maximum.CompareTo(ZNumber) < 0)
-                maximum = ZNumber;
-            return maximum;
-        }
-        public string FindMaximumNumber(string XNumber, string YNumber, string ZNumber)
-        {
-            string maximum = XNumber;
-            if (maximum.CompareTo(YNumber) < 0)
-                maximum = YNumber;
-            if (maximum.CompareTo(ZNumber) < 0)
-                maximum = ZNumber;
-            return maximum;
-        }
+        //public float FindMaximumNumber(float XNumber, float YNumber, float ZNumber)
+        //{
+        //    float maximum = XNumber;
+        //    if (maximum.CompareTo(YNumber) < 0)
+        //        maximum = YNumber;
+        //    if (maximum.CompareTo(ZNumber) < 0)
+        //        maximum = ZNumber;
+        //    return maximum;
+        //}
+        //public string FindMaximumNumber(string XNumber, string YNumber, string ZNumber)
+        //{
+        //    string maximum = XNumber;
+        //    if (maximum.CompareTo(YNumber) < 0)
+        //        maximum = YNumber;
+        //    if (maximum.CompareTo(ZNumber) < 0)
+        //        maximum = ZNumber;
+        //    return maximum;
+        //}
     }
 }
--- TestMaximum/TestMaximumNUnitTest/MaximumTest.cs
+++ TestMaximum/TestMaximumNUnitTest/MaximumTest.cs
@@ -10,21 +10,21 @@ namespace TestMaximumNUnitTest
         public void GivenMaximumNumber_WhenAtFirstPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            int maximumNumber = maximum.FindMaximumNumber(4, 3, 1);
+            int maximumNumber = maximum.FindMaximumNumber<int>(4, 3, 1);
             Assert.AreEqual(4, maximumNumber);
         }
         [Test]
         public void GivenMaximumNumber_WhenAtSecondPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            int maximumNumber = maximum.FindMaximumNumber(3, 4, 1);
+            int maximumNumber = maximum.FindMaximumNumber<int>(3, 4, 1);
             Assert.AreEqual(4, maximumNumber);
         }
         [Test]
         public void GivenMaximumNumber_WhenAtThirdPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            float maximumNumber = maximum.FindMaximumNumber(1f, 3f, 4f);
+            float maximumNumber = maximum.FindMaximumNumber<float>(1f, 3f, 4f);
             Assert.AreEqual(4f, maximumNumber);
         }
 
@@ -32,42 +32,42 @@ namespace TestMaximumNUnitTest
         public void GivenFlotMaximumNumber_WhenAtFirstPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            float maximumNumber = maximum.FindMaximumNumber(4f, 3f, 1f);
+            float maximumNumber = maximum.FindMaximumNumber<float>(4f, 3f, 1f);
             Assert.AreEqual(4f, maximumNumber);
         }
         [Test]
         public void GivenFlotMaximumNumber_WhenAtSecondPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            float maximumNumber = maximum.FindMaximumNumber(3f, 4f, 1f);
+            float maximumNumber = maximum.FindMaximumNumber<float>(3f, 4f, 1f);
             Assert.AreEqual(4f, maximumNumber);
         }
         [Test]
         public void GivenFlotMaximumNumber_WhenAtThirdPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            float maximumNumber = maximum.FindMaximumNumber(1f, 3f, 4f);
+            float maximumNumber = maximum.FindMaximumNumber<float>(1f, 3f, 4f);
             Assert.AreEqual(4f, maximumNumber);
         }
         [Test]
         public void GivenStringMaximumNumber_WhenAtFirstPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            string maximumNumber = maximum.FindMaximumNumber("C", "A", "B");
+            string maximumNumber = maximum.FindMaximumNumber<string>("C", "A", "B");
             Assert.AreEqual("C", maximumNumber);
         }
         [Test]
         public void GivenStringMaximumNumber_WhenAtSecondPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            string maximumNumber = maximum.FindMaximumNumber("A", "C", "B");
+            string maximumNumber = maximum.FindMaximumNumber<string>("A", "C", "B");
             Assert.AreEqual("C", maximumNumber);
         }
         [Test]
         public void GivenStringMaximumNumber_WhenAtThirdPosition_ReturnTrue()
         {
             TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
-            string maximumNumber = maximum.FindMaximumNumber("B", "A", "C");
+            string maximumNumber = maximum.FindMaximumNumber<string>("B", "A", "C");
             Assert.AreEqual("C", maximumNumber);
         }
 
