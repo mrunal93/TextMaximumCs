204c2d7bc68d3c2da0e0bec4798d3b09b068a962
mrunal93 <mrn26893@gmail.com>
2020-07-25 23:04:19 +0530

    [Mrunal]Add: passed Multi parametr and using short had find maximum value

--- TestMaximum/TestMaximum/MaximumUtility.cs
+++ TestMaximum/TestMaximum/MaximumUtility.cs
@@ -10,13 +10,22 @@ namespace TestMaximum
     {
         E[] inputValue;
 
-        public E FindMaximumNumber(params E[] inputValue) 
+        public MaximumUtility(params E[] inputValue)
+        {
+            this.inputValue = inputValue;
+        }
+
+        public E FindMaximumNumber() 
         {
             Array.Sort(inputValue);
             int maximum = inputValue.Length - 1;
             return inputValue[maximum];
             
         }   
+        public void ToprintMaximumNumber()
+        {
+            Console.WriteLine("The Maximum Number is {0}", inputValue[inputValue.Length-1]);
+        }
     }
 
 }
