f5ee317f5884b3c7e67fc6e32f165adb7fe27b48
mrunal93 <mrn26893@gmail.com>
2020-07-25 06:20:58 +0530

    [Mrunal]Add: Add the inter compaired them

--- TestMaximum/TestMaximumNUnitTest/MaximumTest.cs
+++ TestMaximum/TestMaximumNUnitTest/MaximumTest.cs
@@ -7,9 +7,25 @@ namespace TestMaximumNUnitTest
        
 
         [Test]
-        public void Test1()
+        public void GivenMaximumNumber_WhenAtFirstPosition_ReturnTrue()
         {
-            Assert.Pass();
+            TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
+            int maximumNumber = maximum.FindMaximumNumber(4, 3, 1);
+            Assert.AreEqual(4, maximumNumber);
+        }
+        [Test]
+        public void GivenMaximumNumber_WhenAtSecondPosition_ReturnTrue()
+        {
+            TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
+            int maximumNumber = maximum.FindMaximumNumber(3, 4, 1);
+            Assert.AreEqual(4, maximumNumber);
+        }
+        [Test]
+        public void GivenMaximumNumber_WhenAtThirdPosition_ReturnTrue()
+        {
+            TestMaximum.MaximumUtility maximum = new TestMaximum.MaximumUtility();
+            int maximumNumber = maximum.FindMaximumNumber(1, 3, 4);
+            Assert.AreEqual(4, maximumNumber);
         }
     }
 }
