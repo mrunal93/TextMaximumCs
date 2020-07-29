a78c60b88b72fa67aaad755d4700eaf1bd020e89
mrunal93 <mrn26893@gmail.com>
2020-07-29 13:35:18 +0530

    [Mrunal]Add: Added day 5 pratic problem

---
+++ Annotation/Annotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Annotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addRecords("143", "kunal", "24", "cake.csv");
+
+        }
+        public static void addRecords(string iD,string name ,string age,string filePath)
+        {
+            
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filePath, true))
+                {
+                    file.WriteLine(iD + "," + name + "," + age);
+                }
+            
+           
+        }
+    }
+}
---
+++ CensusAnalyser/CensusAnalyser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using LumenWorks.Framework.IO.Csv;
+
+namespace CensusAnalyser
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
